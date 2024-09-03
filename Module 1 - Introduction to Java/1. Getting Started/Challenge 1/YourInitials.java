d025b705632a87f5839dcdef3275af19e10bc7c7
Devin North <13654540+dvnno@users.noreply.github.com>
2024-09-03 00:48:46 -0500

    Workbook 1.1 complete

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,14 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+
+        System.out.println("DDDD   N   N");
+        System.out.println("D   D  NN  N");
+        System.out.println("D   D  NN  N");
+        System.out.println("D   D  N N N");
+        System.out.println("D   D  N N N");
+        System.out.println("D   D  N  NN");
+        System.out.println("DDDD   N   N");
+
     }
 }
